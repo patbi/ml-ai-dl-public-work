59426a033330826eebc9e69baac257a1389c0ed8
Patrick <biyagapatrick@gmail.com>
2025-06-09 17:32:51 +0100

    Test MCP Server Tool

---
+++ investors/server.py
@@ -0,0 +1,35 @@
+# Import dependencies
+from mcp.server.fastmcp import FastMCP
+import json
+import requests
+from typing import List
+
+
+# Server created
+mcp = FastMCP('')
+
+# Create the tool
+@mcp.tool()
+def PredictChurn(data:List[dict]) -> str:
+	"""This tool predicts whether an employee will churn or not,
+	Args:
+		data: employee atributes which are used for inference. Example payload
+
+		[{
+			'YearAtCompany':10,
+			'EmployeeSatisfaction':0.99,
+			'Position':'Non-Manager',
+			'Salary':5.0
+		}]
+
+	Returns:
+		str: 1=churn or 0 = no churn"""
+
+	payload = data[0]
+	response = requests.post("http://127.0.0.1:80000", headers={"Accept":"application/json", "Content-Type":"application/json"}, data=json.dumps(payload),
+	)
+
+	return response.json()
+
+if __name__ == '__main__':
+	mcp.run(transport="stdio")
